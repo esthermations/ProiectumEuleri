bbe9bd506604bbdd98fc7e66db0d4bdddb08e4ee
Esther O'Keefe <esthermations@gmail.com>
2022-02-04 00:48:47 +1100

    Problema primum factum!

---
+++ Problema01.pl
@@ -0,0 +1,19 @@
+use Lingua::Romana::Perligata;
+
+adnota Summam multiplorum trium vel quinque quaere.
+
+da meo  Summo       nullum.
+da meis Intervallis nullum tum CMXCIX conscribementum.
+
+per quisque in Intervallis fac sic
+
+    meo Divisibilis_Per_III_Numero da hoc tum III recidementum tum nullum aequalitam.
+    meo Divisibilis_Per_V_Numero   da hoc tum V   recidementum tum nullum aequalitam.
+
+    si Divisibilis_Per_III_Numerum vel Divisibilis_Per_V_Numerum fac sic
+        da Summo Summum tum hoc addementum.
+    cis
+
+cis
+
+scribe Summum tum novumversum egresso.
